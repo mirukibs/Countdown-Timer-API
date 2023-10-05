0eaee67d84b6319004512e01168b29814de8df80
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2023-10-06 02:41:50 +0300

    Initial Commit

---
+++ src/main/java/com/example/Countdown/Timer/API/CountdownTimerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Countdown.Timer.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountdownTimerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownTimerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Countdown/Timer/API/CountdownTimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Countdown.Timer.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountdownTimerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
